9453c4e86812ade1eb5f21c93eca9885b48fef09
Margarita Lavruk <margolav27@yandex.ru>
2022-12-14 04:24:06 +0300

    конечный вид

--- laba1/main.cpp
+++ laba1/main.cpp
@@ -2,13 +2,36 @@
 
 int main()
 {
-	Lines a(0,0);
-	a+=point(0.2, 0.4);
-	a += point(0.4, 0);
-	for (size_t i = 0; i < a.get_size(); i++)
-	{
-		cout << a[i];
-	}
-	cout << a;
-	
+    Lines a(2);
+    Lines b(2);
+    a += point(0.19, -0.7);
+    cout << a[0] << a[1] << a[2] << endl;
+    cout << b[0] << b[1] << endl;
+    a += b;
+    cout << a;
+
+    cout << a[0];
+    a[0] = point(0.3, 0.3);
+    cout << a[0];
+
+    Lines c = point(1, -0.39) + b;
+    cout << c;
+
+    Lines d = c;
+    if (a == d) {
+        cout << "C==D" << endl;
+    }
+    else {
+        cout << "C!=D" << endl;
+    }
+    cout << "Length a: " << a.length() << endl;
+
+    Lines L(0, 0);
+    L += point(0.2, 0.4);
+    L += point(0.4, 0);
+    for (size_t i = 0; i < L.get_size(); i++)
+    {
+        cout << L[i];
+    }
+    cout << L;
 }
